f921275d393c1ce2d873566b9c74a7b63cfb3042
user <user@admin.admin>
2025-11-05 05:27:32 -0500

    pls

---
+++ get-artifacts1.sh
@@ -0,0 +1,13 @@
+curl -L \
+ -H "Accept: application/vnd.github+json" \
+ -H "Authorization: Bearer $(cat token.gh)" \
+ -H "X-GitHub-Api-Version: 2022-11-28" \
+ https://api.github.com/repos/AntoineLetour/graphes/actions/artifacts > gh-artifacts.json
+
+curl -L \
+ -H "Accept: application/vnd.github+json" \
+ -H "Authorization: Bearer $(cat token.gh)" \
+ -H "X-GitHub-Api-Version: 2022-11-28" \
+ https://api.github.com/repos/AntoineLetour/graphes/actions/artifacts/4472154619/zip --output artifact.zip
+
+unzip artifact.zip
